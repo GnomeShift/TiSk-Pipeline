bd5dee5069b24f672e4a9dcda00b6792300a7452
GnomeShift <151649479+GnomeShift@users.noreply.github.com>
2025-10-13 22:37:37 +0300

    fix(back): no acceptable representation

--- src/main/java/com/gnomeshift/tisk/exception/ExceptionDetails.java
+++ src/main/java/com/gnomeshift/tisk/exception/ExceptionDetails.java
@@ -1,10 +1,12 @@
 package com.gnomeshift.tisk.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
+@Data
 public class ExceptionDetails {
     private LocalDateTime timestamp;
     private String message;
