c02af57f7e26daeb7915bd691d1562fc0d192b72
GnomeShift <151649479+GnomeShift@users.noreply.github.com>
2025-12-05 21:56:15 +0300

    refactor(back): remove `@ConfigurationProperties` from `JwtProperties`

--- src/main/java/com/gnomeshift/tisk/auth/JwtProperties.java
+++ src/main/java/com/gnomeshift/tisk/auth/JwtProperties.java
@@ -2,18 +2,16 @@ package com.gnomeshift.tisk.auth;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "jwt")
 @Data
 public class JwtProperties {
     @Value("${jwt.secret}")
     private String secret;
 
     @Value("${jwt.access-token-expiration}")
-    private long accessTokenExpiration = 86400000;
+    private long accessTokenExpiration;
 
     @Value("${jwt.refresh-token-expiration}")
     private long refreshTokenExpiration;
