b3d905491b9503b7fb7e7983463545269dc91082
GnomeShift <151649479+GnomeShift@users.noreply.github.com>
2025-12-05 19:22:08 +0300

    fix(back): `@NotNull` and `@NotBlank` in `PATCH` method

--- src/main/java/com/gnomeshift/tisk/user/UpdateUserDTO.java
+++ src/main/java/com/gnomeshift/tisk/user/UpdateUserDTO.java
@@ -1,7 +1,6 @@
 package com.gnomeshift.tisk.user;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -12,19 +11,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateUserDTO {
-    @NotBlank(message = "Email required")
     @Email(message = "Invalid email format")
     private String email;
 
-    @NotBlank(message = "First name required")
     @Size(min = 2, max = 100, message = "First name must be between 2 and 100 characters")
     private String firstName;
 
-    @NotBlank(message = "Last name required")
     @Size(min = 2, max = 100, message = "Last name must be between 2 and 100 characters")
     private String lastName;
 
-    @NotBlank(message = "Login required")
     @Size(min = 3, max = 50, message = "Login must be between 3 and 50 characters")
     @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Login can only contain letters, numbers and underscores")
     private String login;
@@ -34,7 +29,6 @@ public class UpdateUserDTO {
 
     private String department;
     private String position;
-
     private UserRole role;
     private UserStatus status;
 }
