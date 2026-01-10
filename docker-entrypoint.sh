ce58a45b2c5c5a2deaba8213b8828e5a64754e3c
GnomeShift <151649479+GnomeShift@users.noreply.github.com>
2026-01-10 17:10:32 +0300

    refactor: update deploy files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,13 +2,11 @@
 
 set -e
 
-export API_URL="${API_URL}"
-export APP_TITLE="${APP_TITLE}"
-
 echo "=== Configuration ==="
 echo "API_URL: $API_URL"
 echo "APP_TITLE: $APP_TITLE"
 
-envsubst < /app/dist/config.js > /app/dist/config.js.tmp
-mv /app/dist/config.js.tmp /app/dist/config.js
-exec npm run preview -- --host 0.0.0.0 --port 4173
+envsubst < /srv/config.js > /srv/config.js.tmp
+mv /srv/config.js.tmp /srv/config.js
+
+exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
